8e15d2db7cb7895cfe1e6e45a7bea32cd4155e05
Lorenzo Catalano <catalanolorenzo95@gmail.com>
2021-07-02 15:52:31 +0200

    ex complete

---
+++ script.js
@@ -0,0 +1,22 @@
+const app = new Vue(
+    {
+        el: '#app',
+
+        data: {
+            randomMail: [],
+        },
+
+        mounted(){
+
+            for (let i = 0; i < 10 ; i++) {
+                
+                axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(result => {
+                    this.randomMail.push(result.data.response);
+                })
+
+            }
+        }
+    }
+);
